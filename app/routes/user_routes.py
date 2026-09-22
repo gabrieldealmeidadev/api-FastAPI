0ac9d7c18db7e8c52ca32694eebee65fc910e951
Gabriel/Korver <gabrielkorver76ers@gmail.com>
2026-09-22 15:44:34 -0300

    completed user crud

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database.database import get_db
@@ -11,6 +11,27 @@ router = APIRouter(
     tags=["Users"]
 )
 
+
+@router.get("/", response_model=list[UserResponse])
+def list_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+
+    return users
+
+@router.get("/{user_id}", response_model=UserResponse)
+def get_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    return user
 
 @router.post("/", response_model=UserResponse)
 def create_user(
@@ -28,3 +49,46 @@ def create_user(
     db.refresh(new_user)
 
     return new_user
+
+@router.put("/{user_id}", response_model=UserResponse)
+def update_user(
+    user_id: int,
+    user_data: UserCreate,
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    user.name = user_data.name
+    user.email = user_data.email
+    user.password = user_data.password
+
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+@router.delete("/{user_id}")
+def delete_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    db.delete(user)
+    db.commit()
+
+    return {
+        "message": "User deleted successfully"
+    }    
